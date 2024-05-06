a96ca9c4b99483897efe4db674eca45c82acd317
Jorge Fernandez <georgfdz@gmail.com>
2024-05-06 18:44:46 +0200

    Feat: Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 import Form from './components/Form'
 import './App.css'
+
 
 
 function App() {
@@ -8,9 +10,8 @@ function App() {
   return (
     <>
       <Navbar />
-
+      <Sidebar />
       <Form />
-
     </>
   )
 }
