b27d9567adb6551331ea216692aa8bc76a8ff75a
Jorge Fernandez <georgfdz@gmail.com>
2024-05-07 14:49:14 +0200

    Implemented redux store and sidebar now toggles between open and closed

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,26 +1,50 @@
-import keyboard from '../assets/keyboard.svg'
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { toggleSidebar } from '../redux/sidebarSlice';
+import keyboard from '../assets/purpleKeyboard.svg'
 import menuIcon from '../assets/menuIcon.svg'
 import friendsIcon from '../assets/friendsIcon.svg'
 import chartIcon from '../assets/chartIcon.svg'
 import settingsIcon from '../assets/settingsIcon.svg'
 import questionIcon from '../assets/questionIcon.svg'
 import exitIcon from '../assets/exitIcon.svg'
+import dotsMenuIcon from '../assets/dotsMenuIcon.svg'
 
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const expanded = useAppSelector((state) => state.sidebar.expanded);
 
   return (
-    <div className="side-bar-container">
-      <div className='main-side-icon-container'>
-        <img src={menuIcon} alt="Menu Icon" />
-        <img src={keyboard} alt="Keyboard Icon" />
-        <img src={friendsIcon} alt="Friends Icon" />
-        <img src={chartIcon} alt="Chart Icon" />
+    <div className={`side-bar-container ${expanded ? 'expanded' : ''}`}>
+      <div className={`main-side-icon-container ${expanded ? 'expanded' : ''}`}>
+        <img src={expanded ? dotsMenuIcon : menuIcon} alt="Menu Icon" onClick={() => dispatch(toggleSidebar())}/>
+        <div className='main-side-icon-container-expanded'>
+          <img src={keyboard} alt="Keyboard Icon" />
+          {expanded && <span>MAIN HUB</span>}
+        </div>
+        <div className='main-side-icon-container-expanded'>
+          <img src={friendsIcon} alt="Friends Icon" />
+          {expanded && <span>FRIENDS</span>}
+        </div>
+        <div className='main-side-icon-container-expanded'>
+          <img src={chartIcon} alt="Chart Icon" />
+          {expanded && <span>STATS</span>}
+        </div>
       </div>
-      <div className="bottom-side-icon-container">
-        <img src={settingsIcon} alt="Settings Icon" />
-        <img src={questionIcon} alt="Question Icon" />
-        <img src={exitIcon} alt="Exit Icon" />
+
+      <div className={`bottom-side-icon-container ${expanded ? 'expanded' : ''}`}>
+        <div className='main-side-icon-container-expanded'>
+          <img src={settingsIcon} alt="Settings Icon" />
+          {expanded && <span>SETTINGS</span>}
+        </div>
+        <div className='main-side-icon-container-expanded'>
+          <img src={questionIcon} alt="Question Icon" />
+          {expanded && <span>HELP CENTER</span>}
+        </div>
+        <div className='main-side-icon-container-expanded'>
+          <img src={exitIcon} alt="Exit Icon" />
+          {expanded && <span>LOG OUT</span>}
+        </div>
       </div>
     </div>
   )
