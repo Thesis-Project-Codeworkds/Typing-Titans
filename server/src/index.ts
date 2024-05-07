24e586fd3f4d2f1eb4078fb9183da9bb4f7150e4
Elkay02 <lucienkayrouz@gmail.com>
2024-05-07 10:53:40 +0200

    update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,10 @@ import app from './app';
 
 config();
 
-const HOST = process.env.HOST || 'localhost';
+// const HOST = process.env.HOST || 'localhost';
 const p = parseInt(process.env.PORT || '');
 const PORT = Number.isInteger(p) ? p : 3009;
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server listening on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
