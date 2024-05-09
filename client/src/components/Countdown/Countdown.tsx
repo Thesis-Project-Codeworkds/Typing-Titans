b1581dab956d5e6bb4eb650fb12153f5d1bec430
Mio <mio.kokosson@gmail.com>
2024-05-09 17:40:32 +0200

    Add navigation to competition page

--- client/src/components/Countdown/Countdown.tsx
+++ client/src/components/Countdown/Countdown.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import socket from '../../socket';
 
@@ -7,6 +8,13 @@ const COUNTDOWN: string[] = ['5', '4', '3', '2', '1', 'Go'];
 const Countdown: React.FC = () => {
 
   const [countdown, setCountdown] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (countdown === 'Go') {
+      setTimeout(() => navigate('/competition'), 1000);
+    }
+  }, [ countdown, navigate ]);
 
   socket.on('countdown', () => {
     COUNTDOWN.map((count, index) => setTimeout(() => {
