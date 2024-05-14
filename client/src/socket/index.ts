7d60aa0739dec59c3b30cc2200241de1e551c102
Mio <mio.kokosson@gmail.com>
2024-05-14 15:41:45 +0200

    Adapt server domain for production

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const SERVER_DOMAIN = 'http://localhost:3000';
+const SERVER_DOMAIN = import.meta.env.DEV
+  ? import.meta.env.VITE_DEV_SERVER_DOMAIN || 'http://localhost:3000'
+  : import.meta.env.VITE_PROD_SERVER_DOMAIN;
 
 const socket = io(SERVER_DOMAIN);
 
