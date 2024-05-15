8534e746067747e5d471601b40307aa91779a47c
Elkay02 <lucienkayrouz@gmail.com>
2024-05-15 14:49:47 +0200

    setup calendar

--- client/src/components/DailyMainCard/DailyMainCard.tsx
+++ client/src/components/DailyMainCard/DailyMainCard.tsx
@@ -1,7 +1,21 @@
-import "./DailyMainCard.css";
+import { useState } from 'react';
+import { LocalizationProvider, DateCalendar } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import './DailyMainCard.css';
+import dayjs from 'dayjs';
 
 const DailyMainCard = () => {
-	return <></>;
+	const [value, setValue] = useState(dayjs());
+
+	return (
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<DateCalendar
+				value={value}
+				onChange={newValue => setValue(newValue)}
+				className="customCalendar"
+			/>
+		</LocalizationProvider>
+	);
 };
 
 export default DailyMainCard;
