7a37523adeae2018da609bc01427a62e8562b430
Jorge Fernandez <georgfdz@gmail.com>
2024-05-15 11:59:08 +0200

    Add cards to competitive section

--- client/src/components/MainCard/MainCard.tsx
+++ client/src/components/MainCard/MainCard.tsx
@@ -1,23 +1,55 @@
-import { Link } from 'react-router-dom';
-import './MainCard.css'
-
+import { Link } from "react-router-dom";
+import "./MainCard.css";
 
 interface MainCardProps {
-  title: string;
-  imgSrc1?: string;
-  imgSrc2?: string;
+	title: string;
+	imgSrc1?: string;
+	imgSrc2?: string;
+	imgSrc3?: string;
 }
 
-const MainCard = ({ title, imgSrc1, imgSrc2 }: MainCardProps) => {
-
-  return (
-  <div className="card-container">
-    <h2 className='card-title'>{title}</h2>
-      {imgSrc1 && <Link to="/lobby/typing" className='cuac'><img src={imgSrc1} alt='competition icon' className='competition-card-svg' /></Link>}
-      {imgSrc2 && <Link to="/lobby/shortcut" className='cuac'><img src={imgSrc2} alt='competition icon' className='competition-card-svg' /></Link>}
-  </div>
-  )
-
-}
+const MainCard = ({ title, imgSrc1, imgSrc2, imgSrc3 }: MainCardProps) => {
+	return (
+		<div className="card-container">
+			<h2 className="card-title">{title}</h2>
+			{imgSrc1 && (
+				<>
+					<p className="typing-race-title">Typing Race</p>
+					<Link to="/lobby/typing" className="typing-race-card">
+						<img
+							src={imgSrc1}
+							alt="competition card"
+							className="competition-card-svg"
+						/>
+					</Link>
+				</>
+			)}
+			{imgSrc2 && (
+				<>
+					<p className="shortcut-race-title">Shortcut Race</p>
+					<Link to="/lobby/shortcut" className="shortcut-race-card">
+						<img
+							src={imgSrc2}
+							alt="competition card"
+							className="competition-card-svg"
+						/>
+					</Link>
+				</>
+			)}
+			{imgSrc3 && (
+				<>
+					<p className="coming-soon-title">Coming Soon!!!</p>
+					<Link to="/lobby/shortcut" className="coming-soon-card">
+						<img
+							src={imgSrc3}
+							alt="competition card"
+							className="competition-card-svg"
+						/>
+					</Link>
+				</>
+			)}
+		</div>
+	);
+};
 
 export default MainCard;
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,21 +1,27 @@
-import Navbar from '../components/Navbar/Navbar'
-import Sidebar from '../components/Sidebar/Sidebar'
-import MainCard from '../components/MainCard/MainCard'
-import CompeteCardSVG from '../assets/CompeteCard.svg'
-import ShortcutSVG from '../assets/shortcut.svg'
+import Navbar from "../components/Navbar/Navbar";
+import Sidebar from "../components/Sidebar/Sidebar";
+import MainCard from "../components/MainCard/MainCard";
+import CompeteCardSVG from "../assets/CompeteCard.svg";
+import ShortcutSVG from "../assets/shortcut.svg";
+import ComingSoonSVG from "../assets/comingSoon.svg";
 
 const LandingPage = () => {
-  return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <div>
-        <MainCard title="Training" />
-        <MainCard title="Competitive" imgSrc1={CompeteCardSVG} imgSrc2={ShortcutSVG}/>
-        <MainCard title="Daily Challenges" />
-      </div>
-    </div>
-  );
-}
+	return (
+		<div>
+			<Navbar />
+			<Sidebar />
+			<div>
+				<MainCard title="Training" />
+				<MainCard
+					title="Competitive"
+					imgSrc1={CompeteCardSVG}
+					imgSrc2={ShortcutSVG}
+					imgSrc3={ComingSoonSVG}
+				/>
+				<MainCard title="Daily Challenges" />
+			</div>
+		</div>
+	);
+};
 
 export default LandingPage;
