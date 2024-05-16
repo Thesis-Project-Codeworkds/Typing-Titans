64c9694b1c06767b44b5838dec4cf6362ef6dad3
Mio <mio.kokosson@gmail.com>
2024-05-16 13:34:57 +0200

    Allow multiple challengers for competitions

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -6,9 +6,22 @@ import { fetchMovies, fetchShortSentence, fetchShortcuts } from './services/api-
 
 let readyPlayers: string[] = [];
 let userNames: any = {};
+let userImages: any = {};
+let playerMinimum: number = 2;
 
 const getUserName = (socketId: string) => {
   return userNames[socketId] ? userNames[socketId] : 'Anonymous';
+}
+
+const getUserImage = (socketId: string) => {
+  return userImages[socketId] ? userImages[socketId] : '';
+}
+
+const getReadyPlayers = () => {
+  return readyPlayers.map(id => ({
+    username: getUserName(id),
+    userimage: getUserImage(id),
+  }));
 }
 
 const socket = (server: HttpServer) => {
@@ -38,17 +51,41 @@ const socket = (server: HttpServer) => {
       userNames[socket.id] = username;
     });
 
+    socket.on('set userimage', (userimage: string) => {
+      userImages[socket.id] = userimage;
+    });
+
+    socket.on('increase minimum', () => {
+      playerMinimum++;
+      io.emit('minimum', playerMinimum);
+    });
+
+    socket.on('decrease minimum', () => {
+      playerMinimum = playerMinimum > 1 ? playerMinimum - 1 : playerMinimum;
+      io.emit('minimum', playerMinimum);
+    });
+
     socket.on('send-message', (msg: string) => {
       const name = getUserName(socket.id);
       socket.broadcast.emit('receive-message', msg, name + ': ')
     });
+
+    socket.on('get ready players', () => {
+      socket.emit('readyPlayers', getReadyPlayers());
+    });
+
+    socket.on('get minimum', () => {
+      socket.emit('minimum', playerMinimum);
+    })
 
     socket.on('isReady', async () => {
       readyPlayers = readyPlayers.includes(socket.id)
         ? readyPlayers.filter(id => id != socket.id)
         : [...readyPlayers, socket.id];
 
-      if (readyPlayers.length > 1) {
+      io.emit('readyPlayers', getReadyPlayers());
+
+      if (readyPlayers.length >= playerMinimum) {
         io.emit('countdown');
         io.emit('sentence', await fetchShortSentence());
         io.emit('shortcuts', await fetchShortcuts());
